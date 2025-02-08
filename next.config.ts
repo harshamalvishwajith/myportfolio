8f6ba41ffd678f2b8e6e031cfbd4488f35349b0c
Harshamal Vishwajith <it22219916@my.sliit.lk>
2025-02-08 07:17:45 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/myportfolio", // ✅ Add this
+  output: "export", // ✅ Add this
   images: {
     unoptimized: true, // ✅ Required for GitHub Pages
   },
