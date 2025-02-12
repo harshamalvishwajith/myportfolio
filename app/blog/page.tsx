776ba4b6547ef359f9b447c38c41b7b2d8a2fb9a
Harshamal Vishwajith <it22219916@my.sliit.lk>
2025-02-12 19:13:38 +0530

    .

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <section>
+    <section className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
       <BlogPosts />
     </section>
